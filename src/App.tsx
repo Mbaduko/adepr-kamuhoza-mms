0b3d7c71072ac4d97431a45320f884c151153dd9
mbaduko <nsengiyumvaclement247@gmail.com>
2025-08-09 16:23:15 +0200

    feat: Standardized codes

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,8 @@ const App = () => (
           <BrowserRouter>
             <Routes>
               {/* Public Routes */}
-              <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Landing />} />
+              <Route path="/" element={<Landing />} />
+              <Route path="/login" element={<Login />} />
               <Route path="/certificate-process" element={<CertificateProcess />} />
               
               {/* Protected Routes */}
